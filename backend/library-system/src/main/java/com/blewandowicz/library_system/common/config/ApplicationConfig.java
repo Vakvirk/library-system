3695b80a5ef2ee13ebafca94a5febbf60ca57eb9
Bartosz Lewandowicz <bartosz.lewandowicz@gmail.com>
2025-09-01 11:30:20 +0200

    Dodano filtr JWT, security config i app config

---
+++ backend/library-system/src/main/java/com/blewandowicz/library_system/common/config/ApplicationConfig.java
@@ -0,0 +1,53 @@
+package com.blewandowicz.library_system.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.blewandowicz.library_system.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    // Email z bazy jako username w celach uwierzytelniania
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return (String username) -> userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Nie znaleziono użytkownika w trakcie uwierzytelniania"));
+    }
+
+    // Szyforwanie hasła
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Manager uwierzytelniania
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    // Ustawienia providera (pobiera usernamea (email) i szyfruje hasło)
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
--- backend/library-system/src/main/java/com/blewandowicz/library_system/common/config/SecurityConfig.java
+++ backend/library-system/src/main/java/com/blewandowicz/library_system/common/config/SecurityConfig.java
@@ -1,5 +1,47 @@
 package com.blewandowicz.library_system.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
+import com.blewandowicz.library_system.common.filters.JWTAuthFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    // TODO: csrf
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 1. Nagłowki: referrerPolicy - ile info o nadawcy widać przy przkierowaniu
+        // 2. AuthRequest: ogólnodostępne endpointy (requestMatchers().permitAll), inne
+        // do uwierzytelniania (anyRequest().authenticated())
+        // 3. Sesja: bez sesji, STATELESS
+        // 4. AuthProvider ze springa
+        // 5. Ddodanie customowego filtra jwt przed standardowym uwierzytelnianiem
+
+        http
+                .headers(headers -> headers.referrerPolicy(
+                        referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER)))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 
 }
